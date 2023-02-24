a11197c60e5a1efa688982636b0623841dd5d6e2
Viieyeon <98655811+Viieyeon@users.noreply.github.com>
2023-02-24 21:25:40 +0300

    master

---
+++ asset/js/main.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+      },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  
+  });
+
+var swiper_2 = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    });
