03dda9f4fb2c71897cdadd102a44efb15034a8a6
Viieyeon <98655811+Viieyeon@users.noreply.github.com>
2023-03-16 00:17:49 +0300

    adaptiv

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,3 +1,12 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobaile = document.querySelector('.menu-mobile');
+
+menuBtn.addEventListener('click', () => {
+  menuMobaile.classList.toggle('menu--open');
+})
+
+
+
 const swiper = new Swiper('.swiper', {
     loop: true,
 
@@ -14,6 +23,35 @@ const swiper = new Swiper('.swiper', {
 var swiper_2 = new Swiper(".mySwiper", {
       slidesPerView: 3,
       spaceBetween: 30,
+      breakpoints: {
+        // when window width is >= 640px
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+
+        992: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 30
+        },
+
+        576: {
+          slidesPerView: 1,
+          spaceBetween: 30
+        },
+
+        300: {
+          slidesPerView: 1,
+          spaceBetween: 30
+        },
+
+
+      },
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
