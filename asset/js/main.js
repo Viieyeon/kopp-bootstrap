45a8edc883381199ef2f96d76b2f4f40b7f5d271
BroShow4ik <gorelovsergey13@gmail.com>
2023-03-17 21:10:15 +0600

    Swiper js

--- asset/js/main.js
+++ asset/js/main.js
@@ -8,57 +8,57 @@ menuBtn.addEventListener('click', () => {
 
 
 const swiper = new Swiper('.swiper', {
-    loop: true,
+  loop: false,
 
-    pagination: {
-        el: ".swiper-pagination",
-      },
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-  
-  });
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+});
 
 var swiper_2 = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  breakpoints: {
+    // when window width is >= 640px
+    1200: {
       slidesPerView: 3,
-      spaceBetween: 30,
-      breakpoints: {
-        // when window width is >= 640px
-        1200: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        },
+      spaceBetween: 30
+    },
 
-        992: {
-          slidesPerView: 2,
-          spaceBetween: 30
-        },
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
 
-        768: {
-          slidesPerView: 1,
-          spaceBetween: 30
-        },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
 
-        576: {
-          slidesPerView: 1,
-          spaceBetween: 30
-        },
+    576: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
 
-        300: {
-          slidesPerView: 1,
-          spaceBetween: 30
-        },
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
 
 
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-      
-    });
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+});
